2e4b2ad3ade7c020457edca2a4189b10772656d8
duc12072001 <gaquayancam@gmail.com>
2023-03-07 20:25:36 +0700

    update Status

--- src/components/Status/Status.js
+++ src/components/Status/Status.js
@@ -5,7 +5,6 @@ import { GLOBALTYPES } from "../../redux/actions/globalTypes";
 import { createPost } from "../../redux/actions/postActions";
 
 import { imageShow, videoShow } from "../../utils/imagesShow";
-import SpinLoader from "../Loading/SpinLoader";
 // import Icons from "../Icon/Icons";
 
 const Status = () => {
@@ -28,6 +27,10 @@ const Status = () => {
         payload: { error: "Không có ảnh. Hãy thêm ảnh của bạn!" },
       });
     dispatch(createPost({ content, images, auth }));
+    dispatch({
+      type: GLOBALTYPES.ALERT,
+      payload: { success: "Đã đăng bài viết" },
+    });
     setContent("");
     setImages([]);
     if (tracks) tracks.stop();
@@ -98,87 +101,83 @@ const Status = () => {
 
   return (
     <div className="status_modal">
-      {auth.token ? (
-        <form onSubmit={handleSubmit}>
-          <div className="status_header">Header Status</div>
-          <div className="status_body">
-            <textarea
-              name="content"
-              value={content}
-              placeholder={`${auth.user.username}, Bạn đang nghĩ gì?`}
-              onChange={(e) => setContent(e.target.value)}
-            />
-            {/* <div className="d-flex">
-              <div className="flex-fill"></div>
-              <Icons setContent={setContent} content={content} />
-            </div> */}
-            <div className="show_images">
-              {images.map((img, index) => (
-                <div key={index} id="file_img">
-                  {img.camera ? (
-                    imageShow(img.camera)
-                  ) : img.url ? (
-                    <>
-                      {img.url.match(/video/i)
-                        ? videoShow(img.url)
-                        : imageShow(img.url)}
-                    </>
-                  ) : (
-                    <>
-                      {img.type.match(/video/i)
-                        ? videoShow(URL.createObjectURL(img))
-                        : imageShow(URL.createObjectURL(img))}
-                    </>
-                  )}
-                  <span onClick={() => deleteImages(index)}>&times;</span>
-                </div>
-              ))}
-            </div>
-            {stream && (
-              <div className="stream position-relative">
-                <video
-                  autoPlay
-                  muted
-                  ref={videoRef}
-                  width="100%"
-                  height="100%"
-                />
-
-                <span onClick={handleStopStream}>&times;</span>
-                <canvas ref={refCanvas} style={{ display: "none" }} />
+      <form onSubmit={handleSubmit}>
+        <div className="status_header">Header Status</div>
+        <div className="status_body">
+          <textarea
+            name="content"
+            value={content}
+            placeholder={`${auth.user.username}, Bạn đang nghĩ gì?`}
+            onChange={(e) => setContent(e.target.value)}
+          />
+          {/* <div className="d-flex">
+            <div className="flex-fill"></div>
+            <Icons setContent={setContent} content={content} />
+          </div> */}
+          <div className="show_images">
+            {images.map((img, index) => (
+              <div key={index} id="file_img">
+                {img.camera ? (
+                  imageShow(img.camera)
+                ) : img.url ? (
+                  <>
+                    {img.url.match(/video/i)
+                      ? videoShow(img.url)
+                      : imageShow(img.url)}
+                  </>
+                ) : (
+                  <>
+                    {img.type.match(/video/i)
+                      ? videoShow(URL.createObjectURL(img))
+                      : imageShow(URL.createObjectURL(img))}
+                  </>
+                )}
+                <span onClick={() => deleteImages(index)}>&times;</span>
               </div>
-            )}
+            ))}
+          </div>
+          {stream && (
+            <div className="stream position-relative">
+              <video
+                autoPlay
+                muted
+                ref={videoRef}
+                width="100%"
+                height="100%"
+              />
 
-            <div className="input_images">
-              {stream ? (
-                <button onClick={handleCapture}>Chụp</button>
-              ) : (
-                <>
-                  <button onClick={handleStream}>Chụp ảnh</button>
-                  <div className="file_upload">
-                    <i className="fas fa-image" />
-                    <input
-                      type="file"
-                      name="file"
-                      id="file"
-                      multiple
-                      accept="image/*,video/*"
-                      onChange={handleChangeImages}
-                    />
-                  </div>
-                </>
-              )}
+              <span onClick={handleStopStream}>&times;</span>
+              <canvas ref={refCanvas} style={{ display: "none" }} />
             </div>
+          )}
+
+          <div className="input_images">
+            {stream ? (
+              <button onClick={handleCapture}>Chụp</button>
+            ) : (
+              <>
+                <button onClick={handleStream}>Chụp ảnh</button>
+                <div className="file_upload">
+                  <i className="fas fa-image" />
+                  <input
+                    type="file"
+                    name="file"
+                    id="file"
+                    multiple
+                    accept="image/*,video/*"
+                    onChange={handleChangeImages}
+                  />
+                </div>
+              </>
+            )}
           </div>
-          <div className="status_footer">
-            <button className="btn btn-secondary w-100" type="submit">
-              Post
-            </button>
-          </div>
-        </form>
-      ) : (
-        <SpinLoader />
-      )}
+        </div>
+        <div className="status_footer">
+          <button className="btn btn-secondary w-100" type="submit">
+            Post
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
