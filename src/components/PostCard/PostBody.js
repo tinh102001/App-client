7599efbe8347c037b45dab1f1259ff8e2a8eb9a3
tung <tung@gmail.com>
2023-04-04 17:09:51 +0700

    fix: ui comment

--- src/components/PostCard/PostBody.js
+++ src/components/PostCard/PostBody.js
@@ -1,37 +1,69 @@
-import React from "react";
+import React, { createRef, useRef, useState } from "react";
 import { Carousel } from "react-bootstrap";
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { faArrowRight, faArrowLeft } from "@fortawesome/free-solid-svg-icons";
+
 const PostBody = ({ post }) => {
+  const [refImages] = useState(
+    Array(post.images.length)
+      .fill(null)
+      .map(() => createRef())
+  );
+  const imageRef = useRef();
+  const [maxHeight, setMaxHeight] = useState(0);
+  const getImageWidth = (link) => {
+    let maxHeight = 0;
+    refImages.forEach((el, i) => {
+      let height =
+        (el.current.naturalHeight * imageRef.current.clientHeight) /
+        el.current.naturalWidth;
+      if (maxHeight < height) maxHeight = height;
+    });
+    setMaxHeight(maxHeight);
+  };
+
   return (
     <div className="post-body">
       <div className="post-content">{post.content}</div>
-      <div className="post-images">
+      <div
+        className="post-images"
+        ref={imageRef}
+        style={
+          maxHeight > 0
+            ? {
+                height: maxHeight,
+              }
+            : {}
+        }
+      >
         <Carousel
-        // nextIcon={
-        //   <Button
-        //     style={{
-        //       display: post.images.length < 2 ? "none" : "unset",
-        //     }}
-        //   >
-        //     <FontAwesomeIcon icon={faArrowRight} />
-        //   </Button>
-        // }
-        // prevIcon={
-        //   <Button
-        //     disabled={post.images < 2}
-        //     style={{
-        //       display: post.images < 2 ? "none" : "unset",
-        //     }}
-        //   >
-        //     <FontAwesomeIcon icon={faArrowLeft} />
-        //   </Button>
-        // }
+          nextIcon={
+            <span
+              className="carousel-control-next-icon"
+              style={{
+                display: post.images.length > 1 ? "unset" : "none",
+              }}
+            ></span>
+          }
+          prevIcon={
+            <span
+              className="carousel-control-prev-icon"
+              style={{
+                display: post.images.length > 1 ? "unset" : "none",
+              }}
+            ></span>
+          }
+          interval={null}
         >
           {post.images.map((img, i) => {
             return (
               <Carousel.Item key={i} className="img-post">
-                <img src={img.url} alt={img.url} />
+                <img
+                  src={img.url}
+                  alt={img.url}
+                  onLoad={() => {
+                    getImageWidth();
+                  }}
+                  ref={refImages[i]}
+                />
               </Carousel.Item>
             );
           })}
--- src/components/PostCard/PostHeader.js
+++ src/components/PostCard/PostHeader.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { NavDropdown } from "react-bootstrap";
 import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
