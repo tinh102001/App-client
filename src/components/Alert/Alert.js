23aae4b58d326be9ef27eb06309a3ecbea30b069
tinh102001 <tinhduc312001@gmail.com>
2023-05-08 10:26:29 +0700

    update profile page

--- src/components/Alert/Alert.js
+++ src/components/Alert/Alert.js
@@ -2,16 +2,13 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { GLOBALTYPES } from "../../redux/actions/globalTypes";
 
-// import SpinLoader from "../Loading/SpinLoader";
 import Toast from "./Toast";
-import Splash from "../Loading/Splash";
 
 const Alert = () => {
   const { alert } = useSelector((state) => state);
   const dispatch = useDispatch();
   return (
     <div>
-      {/* {alert.loading && <Splash />} */}
 
       {alert.error && (
         <Toast
